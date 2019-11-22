4469f71894f823d4385107182144151dc071dbe7
TimFau <tim@timfau.com>
2019-11-21 22:39:17 -0500

    Design/Interactivity/Workflow Updates

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,14 +1,34 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var browserSync = require('browser-sync').create();
+var reload = browserSync.reload;
 
 gulp.task('styles', function(done) {
     gulp.src('css/src/*.scss')
         .pipe(sass().on('error', sass.logError))
+        .pipe(concat('styles.min.css'))
         .pipe(gulp.dest('./css/dest/'));
         done();
 });
-
+gulp.task('scripts', function(done) {
+    gulp.src('js/src/*.js')
+    .pipe(uglify())
+    .pipe(concat('scripts.min.js'))
+    .pipe(gulp.dest('js/dest/'));
+    done();
+});
 //Watch task
 gulp.task('default',function() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
     gulp.watch('css/src/*.scss', gulp.series('styles'));
+    gulp.watch('js/src/*.js', gulp.series('scripts'));
+    gulp.watch('css/dest/*.css').on("change", reload);
+    gulp.watch('js/dest/*.js').on("change", reload);
+    gulp.watch('*.html').on("change", reload);
 });
---
+++ js/src/scripts.js
@@ -0,0 +1,20 @@
+VANTA.WAVES({
+    el: "#intro",
+    color: 0x3d454a
+})
+document.addEventListener("DOMContentLoaded", function() {
+    var portfolioSelectors = document.querySelectorAll('#workCategories span');
+    function portfolioClickHandler(e) {
+        var portfolioItems = document.querySelectorAll('.portfolio-item');
+        var targetItems = document.querySelectorAll('.' + e.target.id + '-project');
+        for (var i = 0; i < portfolioItems.length; i++) {
+            portfolioItems[i].classList.remove('show');
+        }
+        for (var i = 0; i < targetItems.length; i++) {
+            targetItems[i].classList.add('show');
+        }
+    }
+    for (var i = 0; i < portfolioSelectors.length; i++) {
+        portfolioSelectors[i].onclick= portfolioClickHandler;
+    }
+})
