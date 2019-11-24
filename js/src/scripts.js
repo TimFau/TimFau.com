7308b4de85dee8cbd306ef83bad0558d4029abfd
TimFau <tim@timfau.com>
2019-11-24 11:33:00 -0500

    Revise/Add Portfolio Items // Minor Design Updates

--- js/src/scripts.js
+++ js/src/scripts.js
@@ -7,11 +7,15 @@ document.addEventListener("DOMContentLoaded", function() {
     function portfolioClickHandler(e) {
         var portfolioItems = document.querySelectorAll('.portfolio-item');
         var targetItems = document.querySelectorAll('.' + e.target.id + '-project');
+        for (var i = 0; i < portfolioSelectors.length; i++) {
+            portfolioSelectors[i].classList.remove('active');
+        }
         for (var i = 0; i < portfolioItems.length; i++) {
             portfolioItems[i].classList.remove('show');
         }
         for (var i = 0; i < targetItems.length; i++) {
             targetItems[i].classList.add('show');
+            document.getElementById(e.target.id).classList.add('active');
         }
     }
     for (var i = 0; i < portfolioSelectors.length; i++) {
